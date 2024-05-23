ba8fa62aceb13e4d6ef736fab93b538ca83c776a
Igor Araújo <igoraraujo@Igors-MacBook-Pro.local>
2024-05-23 14:25:12 -0300

    Finish the scale

--- http-proxy-keda/setup-cluster.bash
+++ http-proxy-keda/setup-cluster.bash
@@ -1,5 +1,9 @@
-### Ingress
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+### Ingress in prod
+helm upgrade --install ingress-nginx ingress-nginx \
+ --repo https://kubernetes.github.io/ingress-nginx \
+ --namespace ingress-nginx --create-namespace
+### Ingress using kind
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 ### Application
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
@@ -7,3 +11,5 @@ helm install keda kedacore/keda --namespace keda --create-namespace
 helm install http-add-on kedacore/keda-add-ons-http --namespace keda
 
 kubectl apply -f k8s/
+
+
