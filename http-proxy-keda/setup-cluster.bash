294b1098c85e0125500dffd244550ba0e0df5d3e
Igor Araújo <igoraraujo@Igors-MacBook-Pro.local>
2024-05-31 15:50:40 -0300

    Add grpc test

--- http-proxy-keda/setup-cluster.bash
+++ http-proxy-keda/setup-cluster.bash
@@ -1,15 +1,24 @@
-### Ingress in prod
-helm upgrade --install ingress-nginx ingress-nginx \
- --repo https://kubernetes.github.io/ingress-nginx \
- --namespace ingress-nginx --create-namespace
+# ### Ingress in prod
+# helm upgrade --install ingress-nginx ingress-nginx \
+#  --repo https://kubernetes.github.io/ingress-nginx \
+#  --namespace ingress-nginx --create-namespace
+
 ### Ingress using kind
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-### Application
+
+### Install Keda
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
 helm install keda kedacore/keda --namespace keda --create-namespace
 helm install http-add-on kedacore/keda-add-ons-http --namespace keda
 
-kubectl apply -f k8s/
+# ### Ngrok 
+# helm repo add ngrok https://ngrok.github.io/kubernetes-ingress-controller
+
+  
+# # ## Aplication
+# # kubectl apply -f k8s/
+
+# grpcurl -plaintext -H "Host: grpc.example.com" lc-igor.ngrok.io:80 list
 
 
